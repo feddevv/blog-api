72a59712a724526ae6c520946a05989c1b0efe55
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-14 14:40:34 +0000

    feat: replace direct error sending with throwing

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { prisma } from '../db/prisma.js';
 import { hash } from 'bcrypt';
+import { HttpError } from '../errors/HttpError.js';
 
 interface RegisterBody {
   username: string;
@@ -20,7 +21,7 @@ export async function register(req: TypedRequestBody<RegisterBody>, res: Respons
   });
 
   if (existingUser) {
-    return res.sendStatus(409);
+    throw new HttpError(409, 'Username or email is already taken');
   }
 
   const hashedPassword = await hash(password, 10);
