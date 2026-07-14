f1855f54659198887f58130e0675915578274071
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-14 14:36:48 +0000

    feat: implement custom error with middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,26 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'dotenv/config';
 import { router as authRouter } from './routes/auth.js';
+import { HttpError } from './errors/HttpError.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+
+app.use((err: HttpError | Error, req: Request, res: Response, next: NextFunction) => {
+  let statusCode: number | null = null;
+  const message = err.message || 'Something went wrong';
+
+  if (err instanceof HttpError) {
+    statusCode = err.statusCode;
+  }
+
+  res.status(statusCode ?? 500).json({
+    message,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
