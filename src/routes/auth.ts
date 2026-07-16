9abc2ed280091ed1fc8be2a518ab8d425e43d9de
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-16 16:38:20 +0000

    refactor: split schemas into separate blocks
    To omit schema mismatch with AuthenticatedRequest, it's better to create separate types

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { login, register } from '../controllers/authController.js';
 import { validator } from '../validation/validator.js';
-import { registerSchema, loginSchema } from '../validation/schemas.js';
+import { registerBodySchema, loginBodySchema } from '../validation/schemas.js';
 
 const router = Router();
 
-router.post('/register', validator({ body: registerSchema.shape.body }), register);
-router.post('/login', validator({ body: loginSchema.shape.body }), login);
+router.post('/register', validator({ body: registerBodySchema }), register);
+router.post('/login', validator({ body: loginBodySchema }), login);
 
 export { router };
