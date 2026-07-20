580c41d654ecf5bb68e979d10533f342f7882d77
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-20 12:19:16 +0000

    fix: update createIdParamsSchema to use generic type parameter

--- src/validation/utils.ts
+++ src/validation/utils.ts
@@ -1,7 +1,7 @@
 import z from 'zod';
 
 // TODO: CHANGE IT SO THAT ID IS A NUMBER AFTER VALIDATION
-export function createIdParamsSchema(paramName: string) {
+export function createIdParamsSchema<K extends string>(paramName: K) {
   return z.object({
     [paramName]: z.coerce
       .number<string>({
@@ -10,5 +10,6 @@ export function createIdParamsSchema(paramName: string) {
       .int('Must be an integer')
       .positive('Must be positive')
       .transform((num) => String(num)),
-  });
+    // TODO: CHANGE TYPE ASSERTION
+  } as Record<K, z.ZodPipe<z.z.ZodCoercedNumber<string>, z.ZodTransform<string, number>>>);
 }
