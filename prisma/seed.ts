5bc2b455e963c531bda8284c304cc17717e47151
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-20 13:00:03 +0000

    feat: extend seeding by adding comments

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,6 +12,7 @@ async function main() {
       username: 'admin',
       password: hashedPassword,
       email: 'admin@gmail.com',
+      role: 'ADMIN',
     },
   });
 
@@ -22,6 +23,22 @@ async function main() {
         'Це контент першого поста. Тут може бути багато тексту про веб-розробку та Express 5.',
       state: 'PUBLISHED',
       userId: admin.id,
+      comments: {
+        create: [
+          {
+            userId: admin.id,
+            content: 'Hello there!',
+          },
+          {
+            userId: admin.id,
+            content: 'Nice!',
+          },
+          {
+            userId: admin.id,
+            content: 'That was really stunning)',
+          },
+        ],
+      },
     },
   });
 
@@ -31,6 +48,18 @@ async function main() {
       content: 'Цей пост ще не готовий, тому він збережений як чернетка.',
       state: 'HIDDEN',
       userId: admin.id,
+      comments: {
+        create: [
+          {
+            userId: admin.id,
+            content: 'What a nice post. Keep going!',
+          },
+          {
+            userId: admin.id,
+            content: 'Nice man!',
+          },
+        ],
+      },
     },
   });
 
