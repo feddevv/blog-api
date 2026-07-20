c5f1b818678b46bb1fb2930e0c26d1638b557627
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-20 17:16:12 +0000

    fix: add optionalAuthenticate middleware to getPostComments

--- src/routes/nestedComments.ts
+++ src/routes/nestedComments.ts
@@ -3,11 +3,11 @@ import { createComment, getPostComments } from '../controllers/commentsControlle
 import { validator } from '../validation/validator.js';
 import { postParamsSchema } from '../validation/postsSchemas.js';
 import { createCommentBodySchema } from '../validation/commentsSchemas.js';
-import { authenticate } from '../middleware/authenticate.js';
+import { authenticate, optionalAuthenticate } from '../middleware/authenticate.js';
 
 const router = Router({ mergeParams: true });
 
-router.get('/', validator({ params: postParamsSchema }), getPostComments);
+router.get('/', validator({ params: postParamsSchema }), optionalAuthenticate, getPostComments);
 router.post('/', validator({ body: createCommentBodySchema }), authenticate, createComment);
 
 export { router };
