31a0e9c7e0e19a271b1d427ea28237821e716960
Fedya Styopochkin <fst20071221@gmail.com>
2026-08-13 13:47:54 +0000

    fix: configure CORS to allow requests from localhost:5173

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,12 @@ const swaggerJSON = YAML.load('openapi.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSON));
 
 // CORS
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173/',
+    credentials: true,
+  }),
+);
 
 // COOKIE PARSER
 app.use(cookieParser());
