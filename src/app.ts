99c0ca32df3d46e973b86a1dd9e01795eae9188e
Fedya Styopochkin <fst20071221@gmail.com>
2026-08-13 13:58:33 +0000

    fix: change allowed cors origin

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSON));
 // CORS
 app.use(
   cors({
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     credentials: true,
   }),
 );
